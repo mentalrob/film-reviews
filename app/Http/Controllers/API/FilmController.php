3c4dcf749619c65063d0796f340cd3fd2aa58bc2
Berat Ozgur <mrbh228@gmail.com>
2021-06-13 14:57:56 +0300

    webservice api

---
+++ app/Http/Controllers/API/FilmController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Film;
+use Http;
+use Illuminate\Http\Request;
+
+class FilmController extends Controller
+{
+    public function index()
+    {
+        return Film::paginate(50);
+    }
+
+    public function omdbQuery(Request $request)
+    {
+        $data = $request->validate([
+            "title" => "required|string"
+        ]);
+
+        $response = Http::get("https://www.omdbapi.com/", [
+            "apikey" => env("OMDB_KEY"),
+            "s" => $data["title"],
+            "type" => "movie"
+        ]);
+
+        return $response->json();
+    }
+}
